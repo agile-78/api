7635eb4f1ec270628dce0b72fae936b8d0a34fed
Thar Lin Shwe <albent2017@gmail.com>
2023-06-19 19:42:47 +0800

    Updated so environment variables are loaded

--- config/constant.ts
+++ config/constant.ts
@@ -1,8 +1,12 @@
+import "dotenv/config";
+
 export const PORT = 3000 || process.env.PORT;
 export const MONGO_PORT = process.env.MONGO_PORT || 27017;
 export const MONGO_IP = process.env.MONGO_IP || "mongo";
 export const MONGO_PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD;
 export const MONGO_USERNAME = process.env.MONGO_INITDB_ROOT_USERNAME;
-export const MONGO_URI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+export const MONGO_URI =
+  process.env.MONGO_URI ||
+  `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 export const JWT_SECRET = process.env.JWT_SECRET || "test";
 export const JWT_LIFETIME = process.env.JWT_LIFETIME || "2 days";
