7c2f659225807b87060d47fb3792571f24479d9c
Thar Lin Shwe <albent2017@gmail.com>
2023-06-20 14:07:18 +0800

    Implemented helper method for generating Model creation with required fields Test

--- tests/utils/helpers.ts
+++ tests/utils/helpers.ts
@@ -1,4 +1,5 @@
 import { assert, expect } from "chai";
+import { Error } from "mongoose";
 
 export function createModelTest<T, S, D>(
   instance: T,
@@ -17,4 +18,21 @@ export function createModelTest<T, S, D>(
   }
 
   expect(instance).to.be.an.instanceOf(Model);
+}
+
+export async function cannotCreateModelWithoutRequiredFieldsTest<T>(
+  model: T & { create: (obj: Object) => Promise<Object> },
+  requiredFields: Array<string> = []
+) {
+  const createModelWihtoutRequiredFields = async () => await model.create({});
+
+  await expect(createModelWihtoutRequiredFields()).to.be.rejectedWith(
+    Error.ValidationError
+  );
+
+  try {
+    await createModelWihtoutRequiredFields();
+  } catch (e: any) {
+    expect(e.errors).to.include.all.keys(requiredFields);
+  }
 }
