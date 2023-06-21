d870ac24bd26172d418d53639819806d8abd8f17
Thar Lin Shwe <albent2017@gmail.com>
2023-06-21 11:45:45 +0800

    Implemented auth controller

---
+++ controllers/auth.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from "express";
+import { User } from "../models/User";
+import { StatusCodes } from "http-status-codes";
+import {
+  BadRequestError,
+  NotFoundError,
+  UnAuthenticatedError,
+} from "../errors";
+
+export async function register(req: Request, res: Response) {
+  const user = await User.create({
+    ...req.body,
+  });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).send({
+    token,
+  });
+}
+
+export async function login(req: Request, res: Response) {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError("Email and password must be provided");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new NotFoundError("Cannot find user");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  if (!isPasswordCorrect) {
+    throw new UnAuthenticatedError("Invalid credentials");
+  }
+
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).send({
+    token,
+  });
+}
