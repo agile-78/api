1ce9a2353f7a2a049bfde0c958620450398d3f07
Thar Lin Shwe <albent2017@gmail.com>
2023-08-25 14:27:14 +0800

    Written test cases for user controllers so oldProfilePic is deleted when a new profilepic is uploaded && refactored code

--- tests/controllers/user.test.ts
+++ tests/controllers/user.test.ts
@@ -3,11 +3,15 @@ import { Request } from "express";
 import chaiAsPromised from "chai-as-promised";
 import {
   NotFoundErrorisThrownForInvalidId,
+  createDummyUser,
+  createDummyUserWithProfilePic,
   createFakeResponse,
 } from "../utils/helpers";
 import { deleteUser, updateUser } from "../../controllers/user";
 import { User, UserModel } from "../../models/User";
 import { StatusCodes } from "http-status-codes";
+import { access } from "fs/promises";
+import { constants } from "fs";
 
 use(chaiAsPromised);
 describe("User controller", () => {
@@ -16,18 +20,36 @@ describe("User controller", () => {
       await NotFoundErrorisThrownForInvalidId(updateUser);
     });
 
+    it("Delete the old profile pic", async () => {
+      let user = await createDummyUserWithProfilePic();
+      let imagePath = user.profilePic as string;
+      const { res } = createFakeResponse((data) => {
+        user = data.user;
+      });
+
+      const req = {
+        params: {
+          id: user.id as unknown as string,
+        },
+        body: {},
+        file: {
+          path: "./tests/assets/new.png",
+        } as any,
+      } as Partial<Request>;
+
+      await expect(updateUser(req as any, res)).to.eventually.fulfilled;
+      expect(user).property("profilePic").to.be.equal("./tests/assets/new.png");
+
+      await expect(access(imagePath, constants.F_OK)).to.be.rejectedWith(Error);
+    });
+
     it("Changes are saved in database", async () => {
-      const userData = {
-        name: "test",
-        email: "test@gamil.com",
-        password: "password123",
-      };
       let updatedUser: UserModel = {} as any;
       const { res, status } = createFakeResponse((data) => {
         updatedUser = data.user;
       });
 
-      const user = await User.create(userData);
+      const user = await createDummyUser();
 
       const mockRequest = {
         body: {
@@ -51,12 +73,7 @@ describe("User controller", () => {
     });
 
     it("User is deleted from database", async () => {
-      const userData = {
-        name: "test",
-        email: "test@gamil.com",
-        password: "password123",
-      };
-      let user = await User.create(userData);
+      let user = await createDummyUser();
       const { res, status } = createFakeResponse();
       const req = {
         params: {
