3096a3d8de1910aa7be08bb8c61d0f4b04ed6472
Thar Lin Shwe <albent2017@gmail.com>
2023-09-01 18:42:06 +0800

    Written test cases for Reward model

---
+++ tests/models/Reward.test.ts
@@ -0,0 +1,36 @@
+import { describe } from "mocha";
+import { IReward, IRewardMethods, Reward } from "../../models/Reward";
+import { use } from "chai";
+import chaiAsPromised from "chai-as-promised";
+
+import {
+  cannotCreateModelWithoutRequiredFieldsTest,
+  createModelTest,
+} from "../utils/helpers";
+
+use(chaiAsPromised);
+
+describe("Reward model", () => {
+  let reward: IReward & IRewardMethods;
+  const RewardData = {
+    title: "test",
+    points: 30,
+    logo: "./tests/assets/profilePic.png",
+  };
+
+  beforeEach(async () => {
+    reward = await Reward.create(RewardData);
+  });
+
+  it("created a Reward and saved successfully", async () => {
+    createModelTest(reward, Reward, RewardData);
+  });
+
+  it("Reward without required fields are not created", async () => {
+    await cannotCreateModelWithoutRequiredFieldsTest(Reward, [
+      "title",
+      "points",
+      "logo",
+    ]);
+  });
+});
