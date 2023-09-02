9b94603755c0dd395741a6443304994222ec7bbb
Thar Lin Shwe <albent2017@gmail.com>
2023-09-02 15:13:37 +0800

    Updated Redemption model to pass test cases

--- models/Redemption.ts
+++ models/Redemption.ts
@@ -3,6 +3,7 @@ import { Model, Schema, model } from "mongoose";
 export interface IRedemption {
   _id: Schema.Types.ObjectId;
   userId: Schema.Types.ObjectId;
+  rewardId: Schema.Types.ObjectId;
   createdAt: Date;
   updatedAt: Date;
 }
@@ -21,6 +22,11 @@ const RedemptionSchema = new Schema<
       type: Schema.Types.ObjectId,
       ref: "User",
       required: [true, "Redemption must have a claimer"],
+    },
+    rewardId: {
+      type: Schema.Types.ObjectId,
+      ref: "Reward",
+      required: [true, "Redemption must have a reward"],
     },
   },
   {
