cac25377e446e5400e079a4ad381876f89727747
Thar Lin Shwe <albent2017@gmail.com>
2023-09-02 18:06:17 +0800

    Implemented RecyclingMaterial Controller

---
+++ controllers/material.ts
@@ -0,0 +1,10 @@
+import { StatusCodes } from "http-status-codes";
+import { RecyclingMaterial } from "../models/RecyclingMaterial";
+import { Request, Response } from "express";
+
+export async function create(req: Request, res: Response) {
+  const reward = await RecyclingMaterial.create(req.body);
+  res.status(StatusCodes.CREATED).send({
+    reward,
+  });
+}
