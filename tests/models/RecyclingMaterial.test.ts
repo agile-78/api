5527e1f65948421d46eda6f1a0d2a8fd2b734289
Thar Lin Shwe <albent2017@gmail.com>
2023-09-02 17:21:22 +0800

    Written test cases for RecycingMaterial model

---
+++ tests/models/RecyclingMaterial.test.ts
@@ -0,0 +1,42 @@
+import { describe } from "mocha";
+import {
+  IRecyclingMaterial,
+  IRecyclingMaterialMethods,
+  RecyclingMaterial,
+} from "../../models/RecyclingMaterial";
+import { use } from "chai";
+import chaiAsPromised from "chai-as-promised";
+
+import {
+  cannotCreateModelWithoutRequiredFieldsTest,
+  createModelTest,
+} from "../utils/helpers";
+
+use(chaiAsPromised);
+
+describe("RecyclingMaterial model", () => {
+  let recyclingMaterial: IRecyclingMaterial & IRecyclingMaterialMethods;
+  const RecyclingMaterialData = {
+    name: "test material",
+    points: 0,
+  };
+
+  beforeEach(async () => {
+    recyclingMaterial = await RecyclingMaterial.create(RecyclingMaterialData);
+  });
+
+  it("created a RecyclingMaterial and saved successfully", async () => {
+    createModelTest(
+      recyclingMaterial,
+      RecyclingMaterial,
+      RecyclingMaterialData
+    );
+  });
+
+  it("RecyclingMaterial without required fields are not created", async () => {
+    await cannotCreateModelWithoutRequiredFieldsTest(RecyclingMaterial, [
+      "name",
+      "points",
+    ]);
+  });
+});
