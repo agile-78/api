2ff27834bf95dc607439f8d1d8bbe9155f5e9b4c
Thar Lin Shwe <albent2017@gmail.com>
2023-09-10 12:40:30 +0800

    Written test cases for queryingUserReferralCounts

--- tests/utils/points.test.ts
+++ tests/utils/points.test.ts
@@ -3,17 +3,18 @@ import { createDummyUser, createDummyUserRewardAndRedemption } from "./helpers";
 import { RecyclingMaterial } from "../../models/RecyclingMaterial";
 import { RecyclingActivity } from "../../models/RecyclingActivity";
 import { expect, use } from "chai";
-import { queryUserPoints } from "../../utils/point";
+import { queryReferredCount, queryUserPoints } from "../../utils/point";
 import chaiAsPromised from "chai-as-promised";
 import { IUser } from "../../models/User";
 import { HydratedDocument } from "mongoose";
 
 use(chaiAsPromised);
 
-describe("QueryUserPoints", () => {
+describe("Query Utils", () => {
   let user: HydratedDocument<IUser>;
+  let user2: HydratedDocument<IUser>;
   let totalPoints = 0;
-  before(async () => {
+  beforeEach(async () => {
     const {
       redemption,
       user: dummyUser,
@@ -34,14 +35,20 @@ describe("QueryUserPoints", () => {
       quantity: 2,
     });
 
-    await createDummyUser({
+    user2 = await createDummyUser({
       referredBy: user._id,
       email: "test2@gmail.com",
     });
 
     totalPoints += activity.quantity * material.points + 10;
   });
-  it("returns the correct point", async () => {
+  it("queryUserPoints the correct point", async () => {
     await expect(queryUserPoints(user._id)).to.eventually.equals(totalPoints);
+  });
+
+  it("queryReferralCount retuns the correct count", async () => {
+    await expect(queryReferredCount(user._id)).to.eventually.equals(1);
+
+    await expect(queryReferredCount(user2._id)).to.eventually.equals(0);
   });
 });
