f149e866276f3f6d52d1a8d4b9217675f6feb691
Thar Lin Shwe <albent2017@gmail.com>
2023-09-17 20:31:19 +0800

    Fixed typo

--- controllers/activity.ts
+++ controllers/activity.ts
@@ -3,8 +3,8 @@ import { Request, Response } from "express";
 import { RecyclingActivity } from "../models";
 
 export async function create(req: Request, res: Response) {
-  const reward = await RecyclingActivity.create(req.body);
+  const activity = await RecyclingActivity.create(req.body);
   res.status(StatusCodes.CREATED).send({
-    reward,
+    activity,
   });
 }
